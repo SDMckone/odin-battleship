71e179fce251caab9d8be5cf8d1045d0589f4af8
SDMckone <seandmckone@gmail.com>
2023-07-22 21:22:45 -0400

    Add gameplay

--- src/index.js
+++ src/index.js
@@ -2,35 +2,21 @@ import Gameboard from "./modules/Gameboard";
 import DomGrid from "./modules/DomGrid";
 
 const placementGrid = document.querySelector("#placement-grid");
-const placementGameBoard = new Gameboard(10, 10);
-const placementDomGrid = new DomGrid(placementGameBoard);
-placementDomGrid.makePlacementGrid();
+const playerGameBoard = new Gameboard(10, 10);
+const playerDomGrid = new DomGrid(playerGameBoard);
+const placementDomGrid = new DomGrid(playerGameBoard, playerDomGrid);
+
+placementDomGrid.makePlacementGrid([5, 4, 3, 2, 2, 1, 1]);
 
 placementGrid.appendChild(placementDomGrid.gridContainer);
 
 const playerSide = document.querySelector("#player-side");
-const playerGameBoard = new Gameboard(10, 10);
-playerGameBoard.placeShipsRandomly([5, 4, 3, 2, 2, 1, 1]);
-const playerDomGrid = new DomGrid(playerGameBoard);
-// for (let i = 0; i < playerGameBoard.shipsAndLocations.length; i += 1) {
-//   for (
-//     let j = 0;
-//     j < playerGameBoard.shipsAndLocations[i].coordsArray.length;
-//     j += 1
-//   ) {
-//     grid = setSquareColor(
-//       grid,
-//       playerGameBoard.shipsAndLocations[i].coordsArray[j][0],
-//       playerGameBoard.shipsAndLocations[i].coordsArray[j][1],
-//       "ship"
-//     );
-//   }
-// }
 
 playerSide.appendChild(playerDomGrid.gridContainer);
 
 const hostileSide = document.querySelector("#hostile-side");
 const hostileGameBoard = new Gameboard(10, 10);
 hostileGameBoard.placeShipsRandomly([5, 4, 3, 2, 2, 1, 1]);
-const hostileDomGrid = new DomGrid(hostileGameBoard);
+const hostileDomGrid = new DomGrid(hostileGameBoard, playerDomGrid);
+hostileDomGrid.makeHostileGrid();
 hostileSide.appendChild(hostileDomGrid.gridContainer);
--- src/modules/DomGrid.js
+++ src/modules/DomGrid.js
@@ -1,13 +1,17 @@
+import AiPlayer from "./AiPlayer";
+
 class DomGrid {
-  constructor(gameboard) {
+  constructor(gameboard, otherDomGrid = null) {
     this.gridContainer = document.createElement("div");
     this.gridContainer.classList.add("grid-container");
+    this.gameboard = gameboard;
+    this.otherDomGrid = otherDomGrid;
 
-    for (let i = 0; i < gameboard.height; i += 1) {
+    for (let i = 0; i < this.gameboard.height; i += 1) {
       const gridRowContainer = document.createElement("div");
       gridRowContainer.classList.add("grid-row-container");
 
-      for (let j = 0; j < gameboard.width; j += 1) {
+      for (let j = 0; j < this.gameboard.width; j += 1) {
         const gridSquare = document.createElement("div");
         gridSquare.classList.add("grid-square");
         gridSquare.classList.add("clear");
@@ -21,7 +25,7 @@ class DomGrid {
 
   setSquareColor(x, y, state) {
     const square = this.gridContainer.children.item(x).children.item(y);
-    square.classList.remove("clear");
+    square.classList.remove("clear", "hit", "miss", "ship", "ship-placement");
 
     switch (state) {
       case "clear":
@@ -44,7 +48,59 @@ class DomGrid {
     }
   }
 
-  makePlacementGrid() {
+  renderShips() {
+    for (let i = 0; i < this.gameboard.shipsAndLocations.length; i += 1) {
+      for (
+        let j = 0;
+        j < this.gameboard.shipsAndLocations[i].coordsArray.length;
+        j += 1
+      ) {
+        this.setSquareColor(
+          this.gameboard.shipsAndLocations[i].coordsArray[j][0],
+          this.gameboard.shipsAndLocations[i].coordsArray[j][1],
+          "ship"
+        );
+      }
+    }
+  }
+
+  makeHostileGrid() {
+    const ai = new AiPlayer(this.otherDomGrid.gameboard);
+
+    for (let i = 0; i < this.gridContainer.children.length; i += 1) {
+      for (
+        let j = 0;
+        j < this.gridContainer.children.item(i).children.length;
+        j += 1
+      ) {
+        const gridSquare = this.gridContainer.children.item(i).children.item(j);
+        gridSquare.classList.add("hostile-side");
+
+        gridSquare.addEventListener("click", () => {
+          if (gridSquare.classList.contains("clear")) {
+            const wasHit = this.gameboard.receiveAttack(i, j);
+            gridSquare.classList.remove("clear");
+
+            if (wasHit) {
+              gridSquare.classList.add("hit");
+            } else {
+              gridSquare.classList.add("miss");
+            }
+
+            ai.makeMove();
+          }
+          if (this.gameboard.allSunk()) {
+            console.log("Won");
+          }
+        });
+      }
+    }
+  }
+
+  makePlacementGrid(shipList) {
+    let horizontal = false;
+    let shipListIndex = 0;
+
     for (let i = 0; i < this.gridContainer.children.length; i += 1) {
       for (
         let j = 0;
@@ -53,12 +109,82 @@ class DomGrid {
       ) {
         const gridSquare = this.gridContainer.children.item(i).children.item(j);
 
-        gridSquare.addEventListener("mouseover", () => {
-          gridSquare.classList.add("ship-placement");
+        const shipPlacementCallback = () => {
+          if (horizontal) {
+            for (let k = 0; k < shipList[shipListIndex]; k += 1) {
+              if (
+                !this.gameboard.isCoordOccupied(i, j + k) &&
+                i < this.gameboard.height &&
+                j + k < this.gameboard.width
+              ) {
+                this.setSquareColor(i, j + k, "ship-placement");
+              }
+            }
+          } else {
+            for (let k = 0; k < shipList[shipListIndex]; k += 1) {
+              if (
+                !this.gameboard.isCoordOccupied(i + k, j) &&
+                i + k < this.gameboard.height &&
+                j < this.gameboard.width
+              ) {
+                this.setSquareColor(i + k, j, "ship-placement");
+              }
+            }
+          }
+        };
+        gridSquare.addEventListener("mouseover", shipPlacementCallback);
+
+        const mouseLeaveCallback = () => {
+          if (horizontal) {
+            for (let k = 0; k < shipList[shipListIndex]; k += 1) {
+              if (
+                !this.gameboard.isCoordOccupied(i, j + k) &&
+                i < this.gameboard.height &&
+                j + k < this.gameboard.width
+              ) {
+                this.setSquareColor(i, j + k, "clear");
+              }
+            }
+          } else {
+            for (let k = 0; k < shipList[shipListIndex]; k += 1) {
+              if (
+                !this.gameboard.isCoordOccupied(i + k, j) &&
+                i + k < this.gameboard.height &&
+                j < this.gameboard.width
+              ) {
+                this.setSquareColor(i + k, j, "clear");
+              }
+            }
+          }
+        };
+        gridSquare.addEventListener("mouseleave", mouseLeaveCallback);
+
+        gridSquare.addEventListener("click", () => {
+          if (
+            this.gameboard.placeShip(shipList[shipListIndex], i, j, horizontal)
+          ) {
+            this.renderShips();
+            shipListIndex += 1;
+            gridSquare.removeEventListener("mouseover", shipPlacementCallback);
+            gridSquare.removeEventListener("mouseleave", mouseLeaveCallback);
+
+            if (shipListIndex === shipList.length) {
+              document.querySelector("#start-screen").style.display = "none";
+              document.body.classList.remove("low-opacity-bg");
+              document
+                .querySelector("#content")
+                .classList.remove("low-opacity");
+              this.otherDomGrid.renderShips();
+            }
+          }
         });
-        gridSquare.addEventListener("mouseleave", () => {
-          gridSquare.classList.remove("ship-placement");
+
+        gridSquare.addEventListener("contextmenu", mouseLeaveCallback);
+        gridSquare.addEventListener("contextmenu", (event) => {
+          event.preventDefault();
+          horizontal = !horizontal;
         });
+        gridSquare.addEventListener("contextmenu", shipPlacementCallback);
       }
     }
   }
